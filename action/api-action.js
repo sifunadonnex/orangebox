ea907a1afd059523ec1de6b16c9df7543c653856
sifuna2002 <sifunado4704@gmail.com>
2024-07-26 12:19:14 +0300

    res

--- action/api-action.js
+++ action/api-action.js
@@ -4,7 +4,7 @@ const jwtSecret = process.env.AUTH_SECRET;
 
 //login
 export const login = async (data) => {
-  const response = await fetch("http://localhost:8000/login", {
+  const response = await fetch("https://orangebox.toolassist.co.ke/login", {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -27,7 +27,7 @@ const fetchWithAuth = async (url, options = {}) => {
 
 // Get users from the server
 export const getUsers = async () => {
-  const response = await fetchWithAuth("http://localhost:8000/users");
+  const response = await fetchWithAuth("https://orangebox.toolassist.co.ke/users");
   const data = await response.json();
   data.forEach((user) => {
     user.resetToken = null;
@@ -39,7 +39,7 @@ export const getUsers = async () => {
 
 // Get user by id
 export const getUserById = async (id) => {
-  const response = await fetchWithAuth(`http://localhost:8000/users/${id}`);
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/users/${id}`);
   const data = await response.json();
   data.resetToken = null;
   data.resetTokenExpiry = null;
@@ -49,14 +49,14 @@ export const getUserById = async (id) => {
 
 // Get user by email
 export const getUserByEmail = async (email) => {
-  const response = await fetchWithAuth(`http://localhost:8000/users/${email}`);
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/users/${email}`);
   const data = await response.json();
   return data;
 };
 
 // Update user
 export const updateUser = async (id, data) => {
-  const response = await fetchWithAuth(`http://localhost:8000/users/${id}`, {
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/users/${id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
@@ -69,7 +69,7 @@ export const updateUser = async (id, data) => {
 
 // Delete user
 export const deleteUser = async (id) => {
-  const response = await fetchWithAuth(`http://localhost:8000/users/${id}`, {
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/users/${id}`, {
     method: "DELETE",
   });
   const data = await response.json();
@@ -78,7 +78,7 @@ export const deleteUser = async (id) => {
 
 // Add user
 export const addUser = async (data) => {
-  const response = await fetchWithAuth(`http://localhost:8000/users`, {
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/users`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -91,14 +91,14 @@ export const addUser = async (data) => {
 
 // Get aircrafts from the server
 export const getAircrafts = async () => {
-  const response = await fetchWithAuth("http://localhost:8000/aircrafts");
+  const response = await fetchWithAuth("https://orangebox.toolassist.co.ke/aircrafts");
   const data = await response.json();
   return data;
 };
 
 // Get aircraft by id
 export const getAircraftById = async (id) => {
-  const response = await fetchWithAuth(`http://localhost:8000/aircrafts/${id}`);
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/aircrafts/${id}`);
   const data = await response.json();
   return data;
 };
@@ -106,7 +106,7 @@ export const getAircraftById = async (id) => {
 // Update aircraft
 export const updateAircraft = async (id, data) => {
   const response = await fetchWithAuth(
-    `http://localhost:8000/aircrafts/${id}`,
+    `https://orangebox.toolassist.co.ke/aircrafts/${id}`,
     {
       method: "PUT",
       headers: {
@@ -121,7 +121,7 @@ export const updateAircraft = async (id, data) => {
 
 // Add aircraft
 export const addAircraft = async (data) => {
-  const response = await fetchWithAuth(`http://localhost:8000/aircrafts`, {
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/aircrafts`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -135,7 +135,7 @@ export const addAircraft = async (data) => {
 // Delete aircraft
 export const deleteAircraft = async (id) => {
   const response = await fetchWithAuth(
-    `http://localhost:8000/aircrafts/${id}`,
+    `https://orangebox.toolassist.co.ke/aircrafts/${id}`,
     {
       method: "DELETE",
     }
@@ -146,28 +146,28 @@ export const deleteAircraft = async (id) => {
 
 // Get flights from the server
 export const getFlights = async () => {
-  const response = await fetchWithAuth("http://localhost:8000/csv");
+  const response = await fetchWithAuth("https://orangebox.toolassist.co.ke/csv");
   const data = await response.json();
   return data;
 };
 
 // Get CSV file
 export const getCsvFile = async (id) => {
-  const response = await fetchWithAuth(`http://localhost:8000/csv/${id}`);
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/csv/${id}`);
   const data = await response.text();
   return data;
 };
 
 // Get flight by id
 export const getFlightById = async (id) => {
-  const response = await fetchWithAuth(`http://localhost:8000/flight/${id}`);
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/flight/${id}`);
   const data = await response.json();
   return data;
 };
 
 // Update flight
 export const updateFlight = async (id, data) => {
-  const response = await fetchWithAuth(`http://localhost:8000/csv/${id}`, {
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/csv/${id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
@@ -180,7 +180,7 @@ export const updateFlight = async (id, data) => {
 
 // Delete flight
 export const deleteFlight = async (id) => {
-  const response = await fetchWithAuth(`http://localhost:8000/csv/${id}`, {
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/csv/${id}`, {
     method: "DELETE",
   });
   const data = await response.json();
@@ -189,7 +189,7 @@ export const deleteFlight = async (id) => {
 
 // Add flight
 export const newFlight = async (data) => {
-  const response = await fetchWithAuth(`http://localhost:8000/csv`, {
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/csv`, {
     method: "POST",
     body: data,
   });
@@ -199,21 +199,21 @@ export const newFlight = async (data) => {
 
 // Get events from the server
 export const getEvents = async () => {
-  const response = await fetchWithAuth("http://localhost:8000/events");
+  const response = await fetchWithAuth("https://orangebox.toolassist.co.ke/events");
   const data = await response.json();
   return data;
 };
 
 // Get event by id
 export const getEventById = async (id) => {
-  const response = await fetchWithAuth(`http://localhost:8000/events/${id}`);
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/events/${id}`);
   const data = await response.json();
   return data;
 };
 
 // Update event
 export const updateEvent = async (id, data) => {
-  const response = await fetchWithAuth(`http://localhost:8000/events/${id}`, {
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/events/${id}`, {
     method: "PUT",
     headers: {
       "Content-Type": "application/json",
@@ -226,7 +226,7 @@ export const updateEvent = async (id, data) => {
 
 // Add event
 export const addEvent = async (data) => {
-  const response = await fetchWithAuth(`http://localhost:8000/events`, {
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/events`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -239,7 +239,7 @@ export const addEvent = async (data) => {
 
 // Delete event
 export const deleteEvent = async (id) => {
-  const response = await fetchWithAuth(`http://localhost:8000/events/${id}`, {
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/events/${id}`, {
     method: "DELETE",
   });
   const data = await response.json();
@@ -248,7 +248,7 @@ export const deleteEvent = async (id) => {
 
 // Get exceedances from the server
 export const getExceedances = async () => {
-  const response = await fetchWithAuth("http://localhost:8000/exceedances");
+  const response = await fetchWithAuth("https://orangebox.toolassist.co.ke/exceedances");
   const data = await response.json();
   return data;
 };
@@ -256,7 +256,7 @@ export const getExceedances = async () => {
 // Get exceedance by id
 export const getExceedanceById = async (id) => {
   const response = await fetchWithAuth(
-    `http://localhost:8000/exceedances/${id}`
+    `https://orangebox.toolassist.co.ke/exceedances/${id}`
   );
   const data = await response.json();
   return data;
@@ -265,7 +265,7 @@ export const getExceedanceById = async (id) => {
 // Get exceedance by flight id
 export const getExceedanceByFlightId = async (id) => {
   const response = await fetchWithAuth(
-    `http://localhost:8000/exceedances/flight/${id}`
+    `https://orangebox.toolassist.co.ke/exceedances/flight/${id}`
   );
   const data = await response.json();
   return data;
@@ -274,7 +274,7 @@ export const getExceedanceByFlightId = async (id) => {
 // Update exceedance
 export const updateExceedance = async (id, data) => {
   const response = await fetchWithAuth(
-    `http://localhost:8000/exceedances/${id}`,
+    `https://orangebox.toolassist.co.ke/exceedances/${id}`,
     {
       method: "PUT",
       headers: {
@@ -289,7 +289,7 @@ export const updateExceedance = async (id, data) => {
 
 // Add exceedance
 export const addExceedance = async (data) => {
-  const response = await fetchWithAuth(`http://localhost:8000/exceedances`, {
+  const response = await fetchWithAuth(`https://orangebox.toolassist.co.ke/exceedances`, {
     method: "POST",
     headers: {
       "Content-Type": "application/json",
@@ -303,7 +303,7 @@ export const addExceedance = async (data) => {
 // Delete exceedance
 export const deleteExceedance = async (id) => {
   const response = await fetchWithAuth(
-    `http://localhost:8000/exceedances/${id}`,
+    `https://orangebox.toolassist.co.ke/exceedances/${id}`,
     {
       method: "DELETE",
     }
